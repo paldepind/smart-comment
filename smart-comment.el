fce12f416b23a12b3d0ca498e47b2875fc536294
paldepind <simonfv@gmail.com>
2016-03-13 12:43:15 +0100

    Add customizeability

--- smart-comment.el
+++ smart-comment.el
@@ -1,5 +1,9 @@
 ;; Smart comment
 
+(setq smart-comment-end-action 'comment-dwim)
+(setq smart-comment-beg-action 'smart-comment-and-mark-line)
+(setq smart-comment-mid-action 'smart-comment-or-uncomment-line)
+
 (defun smart-comment-cleanup ()
   "Remove lines marked for deletion"
   (interactive)
@@ -10,26 +14,26 @@
       (while (re-search-forward (regexp-quote search) nil t)
         (kill-whole-line)))))
 
-(defun smart-comment-or-uncomment-line ()
+(defun smart-comment-or-uncomment-line (arg)
   (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
 
-(defun smart-comment-and-mark-line ()
+(defun smart-comment-and-mark-line (arg)
   (back-to-indentation)
   (let ((ends-with-space (string= " " (substring comment-start -1)))
         (orig-comment-start comment-start))
     (setq comment-start (concat orig-comment-start
                                 (if ends-with-space "DEL:" " DEL:")))
-    (smart-comment-or-uncomment-line)
+    (smart-comment-or-uncomment-line arg)
     (setq comment-start orig-comment-start)))
 
 (defun smart-comment (arg)
   "Smart commenting"
   (interactive "*P")
   (cond ((looking-at "\\s-*$")
-         (comment-dwim arg))
+         (funcall smart-comment-eol-action arg))
         ((let ((orig-point (point)))
            (save-excursion (back-to-indentation) (<= orig-point (point))))
-         (smart-comment-and-mark-line))
-        (t (smart-comment-or-uncomment-line))))
+         (funcall smart-comment-beg-action arg))
+        (t (funcall smart-comment-mid-action arg))))
 
 (provide 'smart-comment)
