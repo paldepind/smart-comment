c4df709f86c5974a3d70f893325363e619e0ff1f
paldepind <simonfv@gmail.com>
2016-03-13 17:18:14 +0100

    Refactor

--- smart-comment.el
+++ smart-comment.el
@@ -4,60 +4,55 @@
 
 (setq smart-comment-end-action 'comment-dwim)
 (setq smart-comment-beg-action 'smart-comment-mark-line)
-(setq smart-comment-mid-action 'smart-comment-or-uncomment-line)
+(setq smart-comment-mid-action 'smart-comment-line)
+
+(defun smart-comment-mark-string ()
+  (concat comment-start
+          (if (string= " " (substring comment-start -1)) "" " ")
+          "DEL:"))
 
 (defun smart-comment-cleanup ()
   "Remove lines marked for deletion"
-  (interactive)
-  (let ((search (concat comment-start
-                        (if (string= " " (substring comment-start -1)) "DEL:" " DEL:"))))
+  (interactive "*")
+  (let ((search (smart-comment-mark-string)))
     (save-excursion
       (beginning-of-buffer)
       (while (re-search-forward (regexp-quote search) nil t)
         (kill-whole-line)))))
 
-(defun smart-comment-or-uncomment-line (arg)
-  "Comment or uncomment a line"
-  (interactive "*P")
-  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
-
 (defun smart-comment-mark-line (arg)
   "Mark a line for deletion"
   (interactive "*P")
-  (back-to-indentation)
-  (let ((ends-with-space (string= " " (substring comment-start -1)))
-        (orig-comment-start comment-start))
-    (setq comment-start (concat orig-comment-start
-                                (if ends-with-space "DEL:" " DEL:")))
-    (smart-comment-or-uncomment-line arg)
-    (setq comment-start orig-comment-start)))
+  (smart-comment-line 4))
 
-(defun smart-comment-mark-region (arg)
+(defun smart-comment-mark-region (beg end arg)
   "Mark a region for deletion"
-  (interactive "*P")
-  (let ((ends-with-space (string= " " (substring comment-start -1)))
-        (orig-comment-start comment-start))
-    (setq comment-start (concat orig-comment-start
-                                (if ends-with-space "DEL:" " DEL:")))
-    (comment-or-uncomment-region (region-beginning) (region-end))
+  (interactive "*r\nP")
+  (let ((orig-comment-start comment-start))
+    (setq comment-start (smart-comment-mark-string))
+    (comment-or-uncomment-region beg end)
     (setq comment-start orig-comment-start)))
 
 (defun smart-comment-is-at-beg ()
   (let ((orig-point (point)))
     (save-excursion (back-to-indentation) (<= orig-point (point)))))
 
-(defun smart-comment-region (arg)
+(defun smart-comment-region (beg end arg)
   "Comment or uncomment a region"
+  (interactive "*r\nP")
+  (if (= 4 (prefix-numeric-value arg)) (smart-comment-mark-region beg end arg)
+    (comment-or-uncomment-region beg end arg)))
+
+(defun smart-comment-line (arg)
+  "Comment or uncomment a line"
   (interactive "*P")
-  (message "%s" (prefix-numeric-value arg))
-  (if (= 4 (prefix-numeric-value arg)) (smart-comment-mark-region arg)
-    (comment-or-uncomment-region (region-beginning) (region-end) arg)))
+  (smart-comment-region (line-beginning-position) (line-end-position) arg))
 
 (defun smart-comment (arg)
   "Smart commenting"
   (interactive "*P")
   (cond ((and mark-active transient-mark-mode)
-         (smart-comment-region arg))
+         (smart-comment-region (region-beginning) (region-end) arg))
         ((looking-at "\\s-*$")
          (funcall smart-comment-eol-action arg))
         ((smart-comment-is-at-beg)
