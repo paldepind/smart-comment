7bf64754e04214364a09663530e232ea89914418
paldepind <simonfv@gmail.com>
2016-03-15 09:44:58 +0100

    Tweaks

--- smart-comment.el
+++ smart-comment.el
@@ -2,6 +2,7 @@
 
 ;; Smart comment
 
+;;* Customization
 (defgroup smart-comment nil
   "Smarter commenting."
   :prefix "smart-comment-"
@@ -28,6 +29,11 @@
           (if (string= " " (substring comment-start -1)) "" " ")
           smart-comment-mark-string))
 
+(defun smart-comment-is-at-beg ()
+  (let ((orig-point (point)))
+    (save-excursion (back-to-indentation) (<= orig-point (point)))))
+
+;;;###autoload
 (defun smart-comment-cleanup ()
   "Remove lines marked for deletion"
   (interactive "*")
@@ -37,11 +43,13 @@
       (while (re-search-forward (regexp-quote search) nil t)
         (kill-whole-line)))))
 
+;;;###autoload
 (defun smart-comment-mark-line (arg)
   "Mark a line for deletion"
   (interactive "*P")
   (smart-comment-line 4))
 
+;;;###autoload
 (defun smart-comment-mark-region (beg end arg)
   "Mark a region for deletion"
   (interactive "*r\nP")
@@ -50,21 +58,20 @@
     (comment-or-uncomment-region beg end)
     (setq comment-start orig-comment-start)))
 
-(defun smart-comment-is-at-beg ()
-  (let ((orig-point (point)))
-    (save-excursion (back-to-indentation) (<= orig-point (point)))))
-
+;;;###autoload
 (defun smart-comment-region (beg end arg)
   "Comment or uncomment a region"
   (interactive "*r\nP")
   (if (= 4 (prefix-numeric-value arg)) (smart-comment-mark-region beg end arg)
     (comment-or-uncomment-region beg end arg)))
 
+;;;###autoload
 (defun smart-comment-line (arg)
   "Comment or uncomment a line"
   (interactive "*P")
   (smart-comment-region (line-beginning-position) (line-end-position) arg))
 
+;;;###autoload
 (defun smart-comment (arg)
   "Smart commenting"
   (interactive "*P")
@@ -73,7 +80,7 @@
         ((and mark-active transient-mark-mode)
          (smart-comment-region (region-beginning) (region-end) arg))
         ((looking-at "\\s-*$")
-         (funcall smart-comment-eol-action arg))
+         (funcall smart-comment-end-action arg))
         ((smart-comment-is-at-beg)
          (funcall smart-comment-beg-action arg))
         (t (funcall smart-comment-mid-action arg))))
